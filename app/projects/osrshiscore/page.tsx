f984e72e76de6928f78975a3855f61a730b8115d
André Åström <andreeastroem@gmail.com>
2024-06-09 00:22:29 +0200

    fix: display no players Still need to ability to add user in an user friendly way

--- app/projects/osrshiscore/page.tsx
+++ app/projects/osrshiscore/page.tsx
@@ -1,6 +1,6 @@
 import { getStatsByGamemode } from "osrs-json-hiscores";
-import PlayerCard from "./PlayerCard/PlayerCard";
 import PlayerCards from "./PlayerCard/PlayerCards";
+import Link from "next/link";
 
 export const dynamic = "force-dynamic";
 
@@ -9,17 +9,28 @@ export default async function Page({
 }: {
   searchParams: { name: string | string[] };
 }) {
-  if (!name || name.length === 0)
-    return (
-      <div className="bg-gradient-to-br relative from-orange-600 to-yellow-600 w-full min-h-screen">
-        <div className="max-w-[600px] mx-auto p-4">
-          <h1 className="text-3xl">OSRS multiscore</h1>
-          <p>No names provided</p>
-        </div>
-      </div>
-    );
+  const hasNoName = !name || name.length === 0;
 
-  console.log("name", name);
+  return (
+    <div className="bg-gradient-to-br relative from-orange-600 to-yellow-600 w-full min-h-screen">
+      <div className="max-w-[600px] mx-auto p-4">
+        <Link href="/projects">🔙</Link>
+        {hasNoName ? <NoUsers /> : <Players name={name} />}
+      </div>
+    </div>
+  );
+}
+
+function NoUsers() {
+  return (
+    <>
+      <h1 className="text-3xl">OSRS multiscore</h1>
+      <p>No names provided</p>
+    </>
+  );
+}
+
+async function Players({ name }: { name: string | string[] }) {
   const usernames = Array.isArray(name) ? name : [name];
 
   const data = await Promise.all(
@@ -32,11 +43,14 @@ export default async function Page({
     })
   );
 
+  return <PlayerCards players={data} />;
+}
+
+function AddUser() {
   return (
-    <div className="bg-gradient-to-br relative from-orange-600 to-yellow-600 w-full min-h-screen">
-      <div className="max-w-[600px] mx-auto ">
-        <PlayerCards players={data} />
-      </div>
+    <div>
+      <input type="text" name="" />
+      <button>Add</button>
     </div>
   );
 }
